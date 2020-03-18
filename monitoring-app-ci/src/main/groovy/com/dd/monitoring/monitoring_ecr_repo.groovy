5d1856bcfc3817410a4ac67221d4abf72f6c9f6a
vivek22117 <vivekmishra@22117>
2020-03-18 12:58:21 +0530

    updated package structure

--- monitoring-app-ci/src/main/groovy/com/dd/monitoring/monitoring_ecr_repo.groovy
+++ monitoring-app-ci/src/main/groovy/com/dd/monitoring/monitoring_ecr_repo.groovy
@@ -4,16 +4,19 @@ import com.dd.monitoring.builder.MonitoringECRJobBuilder
 import javaposse.jobdsl.dsl.JobParent
 
 def factory = this as JobParent
+def listOfEnvironment = ["dev", "prod"]
+def component = "monitoring-ecr-repo-job"
+
 def description = "Pipeline DSL to create ECR repository and build Docker image!"
-def jobName = "monitoring-ecr-repo-job"
 def displayName = "Monitoring ECR Repository Builder Job"
 def branchesName = "*/master"
 def githubUrl = "https://github.com/vivek22117/Grafana-Prometheus-Monitoring-App.git"
 
+
 new MonitoringECRJobBuilder(
         dslFactory: factory,
         description: description,
-        jobName: jobName,
+        jobName: component + "-"+ listOfEnvironment.get(0),
         displayName: displayName,
         branchesName: branchesName,
         githubUrl: githubUrl,
@@ -25,7 +28,7 @@ new MonitoringECRJobBuilder(
 new MonitoringECRJobBuilder(
         dslFactory: factory,
         description: description,
-        jobName: jobName,
+        jobName: component + "-"+ listOfEnvironment.get(1),
         displayName: displayName,
         branchesName: branchesName,
         githubUrl: githubUrl,
