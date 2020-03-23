56aa095685611a48596b6752b392722815d8887b
vivek22117 <vivekmishra@22117>
2020-03-23 11:21:02 +0530

    Refactoring code for ECS Cluster

--- aws-infra/packer/setup-config.sh
+++ aws-infra/packer/setup-config.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Install ECS Agent"
-sudo yum install -y ecs-init
-
-echo ECS_CLUSTER=dev-monitoring-app >> /etc/ecs/ecs.config
-
-
 echo "Install SSM-Agent"
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl enable amazon-ssm-agent
 sudo systemctl start amazon-ssm-agent
-
-echo "Install Docker engine"
-yum update -y
-yum install docker -y
-usermod -aG docker ec2-user
-sudo systemctl start docker
