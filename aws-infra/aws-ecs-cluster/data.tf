21a4a7cef8568e25dffcf3de2ad6105130aeec19
vivek22117 <vivekmishra@22117>
2020-08-12 16:52:40 +0530

    updating env value

--- aws-infra/aws-ecs-cluster/data.tf
+++ aws-infra/aws-ecs-cluster/data.tf
@@ -12,6 +12,17 @@ data "terraform_remote_state" "vpc" {
   }
 }
 
+data "terraform_remote_state" "vpc-resources" {
+  backend = "s3"
+
+  config = {
+    profile = "admin"
+    bucket  = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    key     = "state/${var.environment}/vpc-resources/terraform.tfstate"
+    region  = var.default_region
+  }
+}
+
 data "terraform_remote_state" "backend" {
   backend = "s3"
 
