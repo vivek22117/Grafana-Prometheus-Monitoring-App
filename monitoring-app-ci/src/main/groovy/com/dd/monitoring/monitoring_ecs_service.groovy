2d0b09a703a1f616b2562ad22b27589e5f022638
vivek22117 <vivekmishra@22117>
2020-08-12 17:49:35 +0530

    adding Jenkinsfile for ECR service

---
+++ monitoring-app-ci/src/main/groovy/com/dd/monitoring/monitoring_ecs_service.groovy
@@ -0,0 +1,54 @@
+package com.dd.monitoring
+
+
+import com.dd.monitoring.builder.MonitoringECSServiceJobBuilder
+import javaposse.jobdsl.dsl.JobParent
+
+def factory = this as JobParent
+def listOfEnvironment = ["dev", "qa", "prod"]
+def component = "monitoring-ecs-service-job"
+
+def scriptLocation = 'jenkins/ecs-service/Jenkinsfile'
+def description = "Pipeline DSL to create ECS service, Load Balancer, ASG and Security groups!"
+def displayName = "Monitoring ECS Cluster Builder Job"
+def branchesName = "*/master"
+def githubUrl = "https://github.com/vivek22117/Grafana-Prometheus-Monitoring-App.git"
+
+
+new MonitoringECSServiceJobBuilder(
+        dslFactory: factory,
+        description: description,
+        jobName: component + "-" + listOfEnvironment.get(0),
+        displayName: displayName + " " + listOfEnvironment.get(0),
+        branchesName: branchesName,
+        githubUrl: githubUrl,
+        credentialId: 'github',
+        environment: listOfEnvironment.get(0),
+        scriptPath: scriptLocation
+).build()
+
+
+new MonitoringECSServiceJobBuilder(
+        dslFactory: factory,
+        description: description,
+        jobName: component + "-" + listOfEnvironment.get(1),
+        displayName: displayName + " " + listOfEnvironment.get(1),
+        branchesName: branchesName,
+        githubUrl: githubUrl,
+        credentialId: 'github',
+        environment: listOfEnvironment.get(1),
+        scriptPath: scriptLocation
+).build()
+
+
+new MonitoringECSServiceJobBuilder(
+        dslFactory: factory,
+        description: description,
+        jobName: component + "-" + listOfEnvironment.get(2),
+        displayName: displayName + " " + listOfEnvironment.get(2),
+        branchesName: branchesName,
+        githubUrl: githubUrl,
+        credentialId: 'github',
+        environment: listOfEnvironment.get(2),
+        scriptPath: scriptLocation
+).build()
